09eb26fab077133e553a1ab1011abe4357a9389d
amcode27 <113937687+amcode27@users.noreply.github.com>
2022-09-21 09:13:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,14 +9,14 @@ global.APIs = {
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'RJeaI9hS',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
-global.autoread = false // auto read message
+global.autoread = true // auto read message
 global.autobio = true 
-global.userRegister = false 
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.userRegister = true 
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
@@ -26,30 +26,30 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "☬AMͥcͣoͫde27-Bₒₜ🐍" //namabot kalian
+global.ownername= "🪀◥དAͥdͣhͫeBawor×፝֟͜×ཌ◤" //nama kalian
+global.myweb ="https://amcode27.my.id" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/JasBiruLangit?sub_confirmation=1" //bebas asal jan hapus
+global.myweb2 = "https://bit.ly/CekDISINIIII" //bebas
+global.email = "amcode27@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6285342106390","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6282138037447','6285211112701'] //premium user
+global.owner = ["6282138037447","6285211112701"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282138037447" // nomor wa kalian
+global.ownernomerr = "+6282138037447" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.background_welcome="https://telegra.ph/file/8f24dd81957b7f5b990cb.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.thumbGif = 'https://telegra.ph/file/1a92b2d26f67c29e9fae3.mp4'
+global.packname = '☬AMͥcͣoͫde27-Bₒₜ🐍' //sticker wm ubah
+global.author = 'Di Buat Oleh ☬AMͥcͣoͫde27-Bₒₜ🐍' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
     premium: "10000",
-    free: 100
+    free: 30
 }
 
 global.adventureRPG = 'https://telegra.ph/file/ff94536d69e0f4f3e7b54.jpg'
@@ -114,20 +114,20 @@ global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutpu
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
+global.prefa = ['','!','.','#']
+global.sp = '﴿☞'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
-    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
-	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+    sukses: '🥸Selesai Brooo..',
+    admin: '🥸Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: '🥸Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: '🥸Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '🥸Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: '🥸Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: '🥸Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: '🥸Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳Proses Broo.. Sabarr.',
+	lockCmd: '🥸Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Sugeng Rawuh @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
 let file = require.resolve(__filename)
